a635ea851ddf0965fac5c19d53192a0ab7a77542
Ankush Chadda <contact@aoswebsolutions.com>
2018-08-20 20:03:35 +0200

    Pep8 fixes

--- movies/tests.py
+++ movies/tests.py
@@ -10,30 +10,30 @@ from .api_handler import get_all_artists_data
 class MoviesListTest(TestCase):
 
     async def artists_json_response(*args):
-        return { 'name': 'artist_name',
-                 'gender': 'Female',  'age': 10 }
+        return {'name': 'artist_name',
+                'gender': 'Female',  'age': 10}
 
     async def film_json_response(*args):
         return [
-        {
-            'name': 'Film example name',
-            'people': ['http://example.com/people/ba924631-068e-4436-b6de-f3283fa848f0']
-        },
-        {
-            'name': 'Film example name2',
-            'people': ['http://example.com/people/']
-        }
-    ]
+            {
+                'name': 'Film example name',
+                'people': ['http://example.com/people/ba924631-068e-4436-b6de-f3283fa848f0']
+            },
+            {
+                'name': 'Film example name2',
+                'people': ['http://example.com/people/']
+            }
+        ]
 
     async def test_get_all_artists_data(self):
         artist_response = mock.Mock()
         artist_response.json = self.artists_json_response
-        with patch("aiohttp.client.ClientSession._request", return_value=artist_response) as patched_resp:            
+        with patch("aiohttp.client.ClientSession._request",
+                   return_value=artist_response) as patched_resp:
             artists_data = await get_all_artists_data(await self.film_json_response())
         assert artists_data == {
-            'http://example.com/people/ba924631-068e-4436-b6de-f3283fa848f0': 
+            'http://example.com/people/ba924631-068e-4436-b6de-f3283fa848f0':
                 {'name': 'artist_name', 'gender': 'Female', 'age': 10}}
-
 
     def test_get_movie_list(self):
         client = Client()
@@ -54,5 +54,5 @@ class MoviesListTest(TestCase):
                 }
             ]
             assert response.context['artists'] == {
-            'http://example.com/people/ba924631-068e-4436-b6de-f3283fa848f0': 
+            'http://example.com/people/ba924631-068e-4436-b6de-f3283fa848f0':
                 {'name': 'artist_name', 'gender': 'Female', 'age': 10}}
--- movies/views.py
+++ movies/views.py
@@ -5,13 +5,16 @@ from django.shortcuts import render
 from .utils import memoize
 from .api_handler import get_all_movies_and_artists
 
+
 @memoize
 def get_movies_list(request):
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
-    all_movies_data, artists_data = loop.run_until_complete(get_all_movies_and_artists())
+    all_movies_data, artists_data = loop.run_until_complete(
+        get_all_movies_and_artists())
 
-    response = render(request, 'index.html', {'movies': all_movies_data, 'artists': artists_data})
+    response = render(request, 'index.html',
+                      {'movies': all_movies_data, 'artists': artists_data})
     response['Cache-Control'] = 'max-age=60'
     return response
